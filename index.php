a9def8db7a94bee7f37b835bb3f66d086fc1fb53
Parisxx <parisstassen@gmail.com>
2025-04-04 16:37:46 +0200

    feat: hero front page

--- index.php
+++ index.php
@@ -9,5 +9,35 @@
 </head>
 <body>
     
+
+    <nav class="navbar">
+    <img src="src\img\logo.png" alt="Logo" class="logo">
+    <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li class="dropdown">
+            <a href="#" class="dropbtn">Diensten</a> 
+            <ul class="dropdown-content">
+                <li><a href="#">Webdesign</a></li>
+                <li><a href="#">SEO</a></li>
+                <li><a href="#">Marketing</a></li>
+                <li><a href="#">Webdesign</a></li>
+                <li><a href="#">SEO</a></li>
+                <li><a href="#">Marketing</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
+
+
+
+    <header class="hero">
+    <div class="hero-text">
+        <h1>Welkom</h1>
+        <p>Bij dé installatiespecialist van Zuid-Limburg!</p>
+    </div>
+</header>
+
+
 </body>
 </html>
