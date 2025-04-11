d40f6fa935ba2c7b32435996b7323b7489191672
Parisxx <parisstassen@gmail.com>
2025-04-11 10:00:38 +0200

    refactor: footer aangepast

--- services.php
+++ services.php
@@ -94,23 +94,28 @@ if (!$service) {
 
 
     <footer>
-        <div class="footer-row left">
+        <div class="footer-row">
             <h3>Installatiebedrijf Stassen</h3>
             <p>Groenenweg 42, 6294 ND Vijlen, Nederland</p>
             <p>06 520 875 39</p>
             <p><a href="mailto:info@stassen-installatie.nl">info@stassen-installatie.nl</a></p>
+                <a href="https://www.facebook.com/StassenInstallatiebedrijf" target="_blank">
+                    <svg width="14px" height="14px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
+                        <path fill="white" d="M23.9981 11.9991C23.9981 5.37216 18.626 0 11.9991 0C5.37216 0 0 5.37216 0 11.9991C0 17.9882 4.38789 22.9522 10.1242 23.8524V15.4676H7.07758V11.9991H10.1242V9.35553C10.1242 6.34826 11.9156 4.68714 14.6564 4.68714C15.9692 4.68714 17.3424 4.92149 17.3424 4.92149V7.87439H15.8294C14.3388 7.87439 13.8739 8.79933 13.8739 9.74824V11.9991H17.2018L16.6698 15.4676H13.8739V23.8524C19.6103 22.9522 23.9981 17.9882 23.9981 11.9991Z"/>
+                    </svg>
+                </a>
         </div>
-        <div class="footer-row right">
+        <div class="footer-row">
             <p>KVK: 14068934</p>
             <p>BTW: NL001814572B39</p>
             <p>IBAN: NL37 RABO 0116 0171 12</p>
 
         </div>
-        <div class="footer-info">
-            <p>&copy; 2025 Installatiebedrijf Stassen</p>
-            <p><a href="privacy.php">Privacy verklaring</a></p>
-        </div>
+        <div class="footer-row">
+        <p>&copy; 2025 Installatiebedrijf Stassen</p>
+        <p><a href="privacy.php">Privacy verklaring</a></p>
     </footer>
+
 
 
     <script src="script.js"></script>
